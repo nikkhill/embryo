08d8e96eded4948c0f2c61bda035404e774d74a7
nikkhill <nikhilbhale@live.com>
2016-11-15 20:19:23 +0530

    Delete useles  things

--- templates/view_info.php
+++
@@ -1,6 +0,0 @@
-<div>
-    <?php        
-        print("<p><h2>Username: <strong>{$info[0]["username"]}</strong></h2></p>");
-        print("<p><h2>Phone: <strong>{$info[0]["phone"]}</strong></h2></p>");
-    ?>
-</div>
